4bb3db8c28c2979ee9aaa262bfb2d41e1e6d6341
duracell33 <36210845+duracell33@users.noreply.github.com>
2026-02-26 17:02:41 +0000

    villager attempt idk

--- src/main/java/net/fabricmc/translucency/mixin/FontRendererMixin.java
+++ src/main/java/net/fabricmc/translucency/mixin/FontRendererMixin.java
@@ -1,6 +1,7 @@
 package net.fabricmc.translucency.mixin;
 
 import btw.community.translucency.TranslucencyColorController;
+import com.prupe.mcpatcher.mal.resource.TexturePackAPI;
 import net.minecraft.src.FontRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
@@ -12,27 +13,43 @@ public class FontRendererMixin {
     private int modifyColor(int color) {
         if (!TranslucencyColorController.shouldForceWhite())
             return color;
-        return 0xFFFFFFFF;
+
+        if (color == 4210752 || color == 8421504) {
+            return 0xFFFFFFFF;
+        }
+        return color;
     }
 
     @ModifyVariable(method = "drawStringWithShadow(Ljava/lang/String;III)I", at = @At("HEAD"), argsOnly = true, ordinal = 2, require = 0)
     private int modifyColorShadow(int color) {
         if (!TranslucencyColorController.shouldForceWhite())
             return color;
-        return 0xFFFFFFFF;
+
+        if (color == 4210752 || color == 8421504) {
+            return 0xFFFFFFFF;
+        }
+        return color;
     }
 
     @ModifyVariable(method = "drawString(Ljava/lang/String;IIIZ)I", at = @At("HEAD"), argsOnly = true, ordinal = 2, require = 0)
     private int modifyColorWorker(int color) {
         if (!TranslucencyColorController.shouldForceWhite())
             return color;
-        return 0xFFFFFFFF;
+
+        if (color == 4210752 || color == 8421504) {
+            return 0xFFFFFFFF;
+        }
+        return color;
     }
 
     @ModifyVariable(method = "renderString(Ljava/lang/String;IIIZ)I", at = @At("HEAD"), argsOnly = true, ordinal = 2, require = 0)
     private int modifyColorRender(int color) {
         if (!TranslucencyColorController.shouldForceWhite())
             return color;
-        return 0xFFFFFFFF;
+
+        if (color == 4210752 || color == 8421504) {
+            return 0xFFFFFFFF;
+        }
+        return color;
     }
 }
--- src/main/java/net/fabricmc/translucency/mixin/GuiMerchantMixin.java
+++ src/main/java/net/fabricmc/translucency/mixin/GuiMerchantMixin.java
@@ -10,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiMerchant.class)
 public class GuiMerchantMixin {
 
-    @Inject(method = "drawScreen(IIF)V", at = @At("HEAD"))
-    private void drawScreenHead(int par1, int par2, float par3, CallbackInfo ci) {
+    @Inject(method = "drawGuiContainerForegroundLayer(II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/FontRenderer;drawString(Ljava/lang/String;III)I", ordinal = 2))
+    private void disableAfterTitles(int par1, int par2, CallbackInfo ci) {
         TranslucencyColorController.disableWhiteText();
     }
 
-    @Inject(method = "drawScreen(IIF)V", at = @At("RETURN"))
-    private void drawScreenReturn(int par1, int par2, float par3, CallbackInfo ci) {
+    @Inject(method = "drawGuiContainerForegroundLayer(II)V", at = @At("RETURN"))
+    private void enableAtReturn(int par1, int par2, CallbackInfo ci) {
         TranslucencyColorController.enableWhiteText();
     }
 }
